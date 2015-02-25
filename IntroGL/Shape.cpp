3baebcfdf58683114425cafd71718eea40c38d6d
MattBeaud <Qworp@hotmail.com>
2015-02-24 16:29:20 -0800

    Shape

--- IntroGL/Shape.cpp
+++ IntroGL/Shape.cpp
@@ -1,10 +1,37 @@
 #include "Shape.h"
 
+Shape::Shape(float in_x, float in_y, float in_width, float in_height) {
+	x = in_x;
+	y = in_y;
+	width = in_width;
+	height = in_height;
 
+	glGenBuffers(1, &vbo); //Generates a buffer BRUH
+}
 
 Shape::Shape()
 {
-	
+	x = 0;
+	y = 0;
+	width = 10;
+	height = 10;
+
+	glGenBuffers(1, &vbo); //Generates a buffer BRUH
+
+}
+
+void Shape::syncVbo() {
+	float vertices[] =
+	{
+		//  Position   Color             Texcoords
+		-0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, // Top-left
+		0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  // Top-right
+		0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,// Bottom-left
+	};
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
 
--- IntroGL/Shape.h
+++ IntroGL/Shape.h
@@ -1,5 +1,17 @@
 #ifndef _Shape_h_
 #define _Shape_h_
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <SOIL.h>
+
+//GLEW includes must come before anything else
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
 
 #include <GL/glew.h>
 class Shape
@@ -17,6 +29,7 @@ public:
 	float width;
 	float height;
 	GLuint vbo;
+	
 };
 
 #endif
